26ca8eb88689e04e0b6be65ff7e34c051a7a2e73
rood187 <rood.pavel@gmail.com>
2019-10-20 22:27:16 +0300

    Aga

--- skitt/views.py
+++ skitt/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'skitt/post_list.html', {})
+    posts = Post.objects.filter(moderatin=True)
+    return render(request, 'skitt/post_list.html', {'posts': posts})
