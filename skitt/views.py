cdffaf8cf4a7af5d8d97bdd47426b19d0157b30a
rood187 <rood.pavel@gmail.com>
2019-10-21 08:25:47 +0300

    Aga

--- skitt/views.py
+++ skitt/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(moderatin=True)
@@ -10,3 +10,30 @@ def post_list(request):
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'skitt/post_details.html',{'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.authon = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'skitt/post_new.html', {'form':form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.authon = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'skitt/post_new.html', {'form': form})
