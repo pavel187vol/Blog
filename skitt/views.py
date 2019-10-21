339bc9f633032195dd33c294b3537eb1ce112850
rood187 <rood.pavel@gmail.com>
2019-10-21 08:59:11 +0300

    Aga

--- skitt/views.py
+++ skitt/views.py
@@ -4,8 +4,25 @@ from django.utils import timezone
 from .forms import PostForm
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(moderatin=True)
+    # posts = Post.objects.filter(moderatin=True)
+    posts = Post.objects.filter(published_date__isnull=False).order_by('created_date')
     return render(request, 'skitt/post_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    post.save()
+    return redirect('post_details', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+def post_drafts_list(request):
+    # posts = Post.objects.filter(moderatin=False)
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'skitt/post_drafts_list.html',{'posts': posts})
 
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -17,7 +34,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.authon = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_details', pk=post.pk)
     else:
@@ -31,7 +47,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.authon = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_details', pk=post.pk)
     else:
