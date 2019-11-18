6b0f4f6e7a91f2f06df0473532e5e605d1ed5e59
rood187 <rood.pavel@gmail.com>
2019-11-18 13:24:21 +0300

    Add README.md

--- Skittel/settings.py
+++ Skittel/settings.py
@@ -141,7 +141,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
